dfbbee7745b95f0b40fad6dcfbed203f41e8c364
eljordan23 <saidpablo@outlook.es>
2025-09-09 14:56:38 -0600

    Update 1.1 sistema de ec lineales.py
    agregue titulo, comentarios y modifique la jerarquia de operaciones

--- main/sistema de ec lineales.py
+++ main/sistema de ec lineales.py
@@ -1,3 +1,5 @@
+print("Solucion a la ecuacion de primer grado con el metodo de Cramer")
+#variable(tipodedato)(funcion)(string)
 A1 = float(input("Ingresa A1: "))
 B1 = float(input("Ingresa B1: "))
 C1 = float(input("Ingresa C1: "))
@@ -5,11 +7,11 @@ A2 = float(input("Ingresa A2: "))
 B2 = float(input("Ingresa B2: "))
 C2 = float(input("Ingresa C2: "))
 
-det = A1 * B2 - B1 * A2
-
-if det == 0:
-    print("El sistema no tiene solución única (determinante = 0)")
+determinante = A1 * B2 - B1 * A2
+#condicionales: si el determinante es igual a 0 se imprime la condicion if 
+if determinante == 0:
+    print("no se puede resolver porque el determinante es = 0")
 else:
-    x = (C1 * B2 - B1 * C2) / det
-    y = (A1 * C2 - C1 * A2) / det
+    x = (C1 * B2 - B1 * C2) / determinante
+    y = (A1 * C2 - C1 * A2) / determinante
     print(f"Solución: x = {x:.2f}, y = {y:.2f}")
