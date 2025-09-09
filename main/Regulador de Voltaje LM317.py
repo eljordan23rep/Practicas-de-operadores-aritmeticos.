00b60efe68f69f19f1d71de84a27fcfcaf9709d8
eljordan23 <saidpablo@outlook.es>
2025-09-09 15:12:58 -0600

    Update1.1 Regulador de Voltaje LM317.py

--- main/Regulador de Voltaje LM317.py
+++ main/Regulador de Voltaje LM317.py
@@ -1,9 +1,9 @@
-#regulador de voltaje
-#(variable)(funcion)(string se convierte a un int)
+print("ingrese los valores para calcular el valor de R2 en un regulador de voltaje LM317")
+#(variable)(funcion)(string )
 Vo = float(input("Ingresa el voltaje de salida Vo: "))
 R1 = float(input("Ingresa el valor de R1 en ohms: "))
 
-IADJ = 0.0001  # 100 µA  esto es una variable constate 
+IADJ = 0.0001  #   esto es un valor constate 
 #operador aritmetico
 R2 = (Vo - 1.25) / ((1.25 / R1) + IADJ)
 #imprimir en pantalla con el parametro f-string
